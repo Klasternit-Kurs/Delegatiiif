1d023b15f175d859e43950deabc760bc6ed2f003
Dragan Pepic <d.pepic@hotmail.com>
2020-08-06 18:54:34 +0200

    dfdfg

--- Alarmmm/MainWindow.xaml.cs
+++ Alarmmm/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,45 +29,100 @@ namespace Alarmmm
 			InitializeComponent();
 
 			Protivpozarni pp = new Protivpozarni();
+			DrugiAlarm da = new DrugiAlarm();
 			Vatrogasac v1 = new Vatrogasac();
 			Vatrogasac v2 = new Vatrogasac();
 			Civil c = new Civil();
+
 			pp.PpAlarm += c.Panici;
+			//da.DrugiAlarmm += c.Panici;
+
 			pp.PpAlarm += v1.CujesAlarm;
 			pp.PpAlarm += v2.CujesAlarm;
 
-
 			pp.Zvoni();
+
+			dgm.Click += dgm_Click;
+			dgm2.Click += dgm_Click;
+
+			ObservableCollection<int> oc = new ObservableCollection<int>();
+	
+			oc.CollectionChanged += Promena;
+
+			oc.Add(4);
+			oc.Add(5);
+			oc.Remove(5);
 		}
+
+		public void Promena(object c, NotifyCollectionChangedEventArgs a)
+		{
+			if (a.Action == NotifyCollectionChangedAction.Add)
+				MessageBox.Show(a.NewItems[0].ToString());
+			else if (a.Action == NotifyCollectionChangedAction.Remove)
+				MessageBox.Show("Uklonjeno! : " + a.OldItems[0].ToString());
+		}
+
+		private void dgm_Click(object sender, RoutedEventArgs e)
+		{
+			
+			MessageBox.Show(sender.ToString());
+		}
+	}
+
+	public class AlarmEventArgs : EventArgs
+	{
+		public int temp;
+		public string info; 
 	}
 
 	public class Protivpozarni
 	{
-		public delegate void Alarm(string x);
+		public delegate void Alarm(object KoSalje,
+			AlarmEventArgs  a);
 		public event Alarm PpAlarm;
 
 		public void Zvoni()
 		{
-			PpAlarm?.Invoke("Zdravo :)");
+			PpAlarm?.Invoke(this,  new AlarmEventArgs());
+		}
+	}
+
+	public class DrugiAlarm
+	{
+		public delegate void Alarm(object KoSalje, string x);
+		public event Alarm DrugiAlarmm;
+
+		public void Zvoni()
+		{
+			DrugiAlarmm?.Invoke(this, "Zdravo :)");
 		}
 	}
 
 	public class Civil
 	{
-		public void Panici(string x) { }
+		public void Panici(object govornik, AlarmEventArgs a) 
+		{
+			if (govornik is Protivpozarni pp && a.temp > 7)
+			{
+
+			} else
+			{
+
+			}
+		}
 	}
 
 	public class Vatrogasac
 	{
 		public bool CuoAlarm;
 
-		public void CujesAlarm(string x) => CuoAlarm = true;
+		public void CujesAlarm(object govornik, AlarmEventArgs a) => CuoAlarm = true;
 	}
 
 	public class Policajac
 	{
 		public bool CuoAlarm;
 
-		public void CujesAlarm(string x) => CuoAlarm = true;
+		public void CujesAlarm(object govornik, AlarmEventArgs a) => CuoAlarm = true;
 	}
 }
